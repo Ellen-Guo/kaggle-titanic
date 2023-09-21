d2f59d574ed0c374e11377b57a186b3dc27f4f59
Ellen Guo <elleng18@vt.edu>
2023-09-20 23:19:17 -0400

    Fill in empty values

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 
 train_data = pd.read_csv('Data/train.csv')
+
+## Data Preprocessing
+# Turn into DataFrame
+train_data = pd.DataFrame(train_data)
+# TODO: Find ranges to better fill empty values
+train_data = train_data.replace(r'', np.nan, regex=True)
+
 # Random shuffle and split data into train and validation sets
 train_data, val_data = train_test_split(train_data, test_size=0.2, random_state=42)
 train_targets = train_data['Survived'].values
@@ -11,5 +19,3 @@ val_targets = val_data['Survived'].values
 val_features = val_data.drop(['Survived'], axis=1).values
 print(train_features)
 print(train_targets)
-
-# Data Preprocessing
